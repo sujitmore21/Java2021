fe24c00219ef6f95b40c4fc2d6ea270b4740e1a6
Sujit More <sujitmore21@gmail.com>
2021-02-16 12:38:10 +0530

    // Java OR Operators Logical || and Bitwise |

--- src/Operators.java
+++ src/Operators.java
@@ -22,13 +22,40 @@ public class Operators {
 
         // Java Left Shift Operators <<
 
-        System.out.println(10<<2);//10*2^2-10*4=40
+     /*   System.out.println(10<<2);//10*2^2-10*4=40
         System.out.println(18<<3);//18*2^3-18*8=144
 
         // Java Right Shift Operators <<
 
         System.out.println(10>>2);//10/2^2-10/4=2
         System.out.println(18>>3);//18/2^3-18/8=2
+*/
+        // Java AND Operators Logical && and Bitwise &
+
+       /* int a=10;
+        int b=5;
+        int c=28;
+
+        System.out.println(a<b&&a++<c);
+        System.out.println(a);
+
+        System.out.println(a<b&a++<c);
+        System.out.println(a);*/
+
+        // Java OR Operators Logical || and Bitwise |
+
+        int a=10;
+        int b=5;
+        int c=28;
+
+        System.out.println(a>b||a++<c);// true || true = true
+        System.out.println(a);
+
+        System.out.println(a>b|a++<c);// true | true = true
+        System.out.println(a);
+
+
+
 
     }
 }
