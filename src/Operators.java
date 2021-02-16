38ba4ead21e1fe5421f5c0eed8618acbe81bef4d
Sujit More <sujitmore21@gmail.com>
2021-02-16 11:44:44 +0530

    //Unary Operators Example : ~ and !

--- src/Operators.java
+++ src/Operators.java
@@ -2,13 +2,23 @@ public class Operators {
     public static void main(String[] args) {
 
         //Unary Operators Example : ++ and --
-
+/*
         int x=10;
         System.out.println(x++);//10(11)
         System.out.println(++x);//12
         System.out.println(x--);//12(11)
-        System.out.println(--x);//10
+        System.out.println(--x);//10*/
 
+        //Unary Operators Example : ~ and !
+
+        int a=10;
+        int b=-10;
+        boolean c=true;
+        boolean d=false;
+        System.out.println(~a);//-11 (Value start from 0)
+        System.out.println(~b);//9
+        System.out.println(!c);//false
+        System.out.println(!d);//true
 
     }
 }
