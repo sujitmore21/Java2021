3b7cb2358f1261eb96266e4c7e9186108c40cbeb
Sujit More <sujitmore21@gmail.com>
2021-02-16 10:53:11 +0530

    Java operator Example : ++ and --

---
+++ src/Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        //Unary Operators Example : ++ and --
+
+        int x=10;
+        System.out.println(x++);//10(11)
+        System.out.println(++x);//12
+        System.out.println(x--);//12(11)
+        System.out.println(--x);//10
+
+
+    }
+}
