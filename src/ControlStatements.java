0b4715f700b123c3fe93a4ff12a91e68568dc52d
Sujit More <sujitmore21@gmail.com>
2021-02-16 17:13:43 +0530

    if else Statement overview

--- src/ControlStatements.java
+++ src/ControlStatements.java
@@ -4,11 +4,48 @@ public class ControlStatements {
 
         //if Statement
 
-        int age = 23;
+       /* int age = 23;
         if (age>=18)
             System.out.println("You can apply for driving licence");
         else
             System.out.println("You can not apply for driving licence");
+*/
+
+       /* int marks = 65;
+
+        if (marks<50){
+            System.out.println("fail");
+        }
+        else if (marks>=50 && marks<60){
+            System.out.println("D grade");
+        }
+        else if (marks>=60 && marks<70){
+            System.out.println("C grade");
+        }
+        else if (marks>=70 && marks<80){
+            System.out.println("B grade");
+        }
+        else if (marks>=80 && marks<90){
+            System.out.println("A grade");
+        }
+        else if (marks>=90 && marks<100){
+            System.out.println("A+ grade");
+        }
+        else {
+            System.out.println("Invalid!");
+        }*/
+
+
+        int number = -13;
+        if (number>0){
+            System.out.println("Positive");
+        }
+        else if (number<0){
+            System.out.println("Negative");
+        }
+        else {
+            System.out.println("Zero");
+        }
 
     }
 }
