bedf169e665cadf349b295a7f5a221d0510e3d94
Sujit More <sujitmore21@gmail.com>
2021-02-17 16:04:02 +0530

    Java String overview

---
+++ src/javaString.java
@@ -0,0 +1,28 @@
+public class javaString {
+    public static void main(String[] args) {
+
+        // Java String
+
+  /*      char[] ch = {'r','o','h','a','n'};
+        String s = new String(ch);
+        System.out.println(s);*/
+
+        // Java String provides a lot of methods to perform operations on strings such as
+        // compare(), concat(), equals(), split(), replace(), compareTo(), intern(), substring() etc.
+
+        String name = "rohan";
+
+        System.out.println(name.substring(0,4));
+
+
+
+        //int length = name.length();
+
+        //System.out.println(length);
+
+       // System.out.println(name.charAt(length-1));
+
+//        char ch = name.charAt(0);
+//        System.out.println(ch);
+    }
+}
