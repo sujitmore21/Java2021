8b74a764257561fbaa01eb20e3bcd6907c675079
Sujit More <sujitmore21@gmail.com>
2021-02-18 13:16:12 +0530

    Java Convert binary to hexnumber

--- src/ConversionJava.java
+++ src/ConversionJava.java
@@ -63,13 +63,87 @@ public class ConversionJava {
         System.out.println(date1);*/
 
         // Java Date to String
-
+/*
         Date date = Calendar.getInstance().getTime();
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         String strdate = dateFormat.format(date);
-        System.out.println(strdate);
+        System.out.println(strdate);*/
 
+        // Java Convert String to char
+
+    /*    String name = "rohan";
+
+     *//*   char c = name.charAt(0);
+        System.out.println(c);*//*
+
+        char[] ch = name.toCharArray();
+        for (int i =0; i<name.length();i++){
+            System.out.println(i+"-"+ch[i]);
+        }
+        System.out.println(ch);*/
+
+        // Java Convert char to String
+
+ /*       char c= 'r';
+
+        //String ns = String.valueOf(c);
+        String na = Character.toString(c);
+        System.out.println(na+"ahan");*/
+
+        // Java Convert int to long
+
+      /*  int number = 10;
+        long num = number;
+        System.out.println(num);
+
+        Long ns = new Long(number);
+        System.out.println(ns);
+
+        Long ls = Long.valueOf(number);
+        System.out.println(ls);*/
+
+
+        // Java Convert long to int
+
+        /*long number = 123490;
+        int i = (int) number;
+        System.out.println(i);*/
+
+      /*  Long number = 123432l;
+        int i = number.intValue();
+        System.out.println(i);*/
+
+        // Java Convert char to int
+
+        /*char c = 'i';
+
+        //int a = Integer.parseInt(String.valueOf(c));
+
+        int i = Character.getNumericValue(c);
+    //    int i = r;
+
+        System.out.println(i);*/
+
+        // Java Convert Binary to Decimal
+
+       /* String binarynumber = "1001";
+
+        int decimal = Integer.parseInt(binarynumber,2);
+        System.out.println(decimal);*/
+
+        // Java Convert Decimal to Binary
+
+        /*int number = 10;
+        System.out.println(Integer.toBinaryString(number));
+*/
+        // Java Convert binary to hexnumber
+
+        String binarynumber = "1010";
+        int decimal = Integer.parseInt(binarynumber,2);
+
+        //System.out.println(Integer.toHexString(decimal));
+        System.out.println(Integer.toOctalString(decimal));
 
     }
 }
