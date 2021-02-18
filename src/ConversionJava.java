e5003f4394e4c063a89a3bdcef440fba1b30cdf7
Sujit More <sujitmore21@gmail.com>
2021-02-18 10:03:43 +0530

    Java Convert String to int using Conversion

---
+++ src/ConversionJava.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ConversionJava {
+    public static void main(String[] args) {
+
+        // Java Conversion
+        // Java Convert String to int
+
+   /*     Scanner ns = new Scanner(System.in);
+        String number;
+        System.out.println("Enter the number");
+        number = ns.nextLine();
+        System.out.println(number+10);*/
+
+       /* Scanner ns = new Scanner(System.in);
+        String numberstring;
+        System.out.println("Enter the number");
+        numberstring = ns.nextLine();
+
+        int number = Integer.parseInt(numberstring);
+        System.out.println(number+10);*/
+
+        String numberstring = "10";
+
+        int number = Integer.parseInt(numberstring);
+        System.out.println(number+10);
+    }
+}
